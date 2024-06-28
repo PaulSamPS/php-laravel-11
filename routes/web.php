3a2a92e71a2846651801c9f9f2d4a51ab128da05
Pavel Samoylenko <paulsamv@yandex.ru>
2024-06-28 14:45:23 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'welcome'])->name('welcome');
 
-Route::get('/note', [NoteController::class, 'index'])->name('note.index');
-Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+//Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+//Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+//
+//Route::post('/note', [NoteController::class, 'store'])->name('note.store');
+//
+//Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+//
+//Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+//
+//Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
+//
+//Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
 
-Route::post('/note', [NoteController::class, 'store'])->name('note.store');
-
-Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
-
-Route::get('/note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
-
-Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
-
-Route::delete('/note/{id}', [NoteController::class, 'destroy'])->name('note.destroy');
+Route::resource('note', NoteController::class);
