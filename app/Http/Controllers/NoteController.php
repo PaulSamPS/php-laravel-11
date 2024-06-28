176b2f521ee77d1a3fac0f7c04be068b1ae2d6d0
Pavel Samoylenko <paulsamv@yandex.ru>
2024-06-28 15:54:48 +0300

    crud

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -12,7 +12,8 @@ class NoteController extends Controller
      */
     public function index()
     {
-        return view('note.index');
+        $notes = Note::query()->orderBy('created_at', 'desc')->paginate();
+        return view('note.index', ['notes' => $notes]);
     }
 
     /**
@@ -28,7 +29,15 @@ class NoteController extends Controller
      */
     public function store(Request $request)
     {
-        return 'store';
+        $data = $request->validate([
+            'note' => ['required', 'string'],
+        ]);
+
+        $data['user_id'] = 1;
+
+        $note = Note::create($data);
+
+        return to_route('note.show', $note)->with('message', 'Note created successfully.');
     }
 
     /**
@@ -36,7 +45,7 @@ class NoteController extends Controller
      */
     public function show(Note $note)
     {
-        return view('note.show');
+        return view('note.show', ['note' => $note]);
     }
 
     /**
@@ -44,7 +53,7 @@ class NoteController extends Controller
      */
     public function edit(Note $note)
     {
-        return view('note.edit');
+        return view('note.edit', ['note' => $note]);
     }
 
     /**
--- resources/views/note/edit.blade.php
+++ resources/views/note/edit.blade.php
@@ -1,3 +1,13 @@
-<div>
-    <h1>Edit</h1>
-</div>
+<x-layout>
+    <div class="note-container single-note">
+        <h1>Edit you note</h1>
+        <form action="{{ route('note.update', $note) }}" method="POST" class="note">
+            @csrf
+            <textarea name="note" rows="10" class="note-body" placeholder="Enter you note here">{{ $note->$note }}</textarea>
+            <div class="note-buttons">
+                <a href="" class="note-cancel-button">Cancel</a>
+                <button class="note-submit-button">Submit</button>
+            </div>
+        </form>
+    </div>
+</x-layout>
